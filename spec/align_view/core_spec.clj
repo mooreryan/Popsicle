ea3c8333e68bb68c9ed501d64ab766769fd0f0ab
mooreryan <moorer@udel.edu>
2013-11-22 13:02:30 -0500

    first commit

---
+++ spec/align_view/core_spec.clj
@@ -0,0 +1,7 @@
+(ns align_view.core-spec
+  (:require [speclj.core :refer :all]
+            [align_view.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ src/align_view/core.clj
@@ -0,0 +1,1 @@
+(ns align_view.core)
